ab65e52efa20f048c5811d95257fb91524cc2b19
Boris <boris.ignatov.99@gmail.com>
2023-07-03 16:26:45 +0600

    allowed hosts

--- phoenixtest/integrations/services/api_call.py
+++ phoenixtest/integrations/services/api_call.py
@@ -6,7 +6,7 @@ async def call_api(user: CustomUser):
     async with httpx.AsyncClient() as client:
         body = {'request_id': 'e1477272-88d1-4acc-8e03-7008cdedc81e', 'ClubId': user.club_id, 'Method': 'GetSpecialList',
                 'Parameters': {'ServiceId': ''}}
-        r = await client.post("http://127.0.0.1:8000/users/integration", data=body,
+        r = await client.post("http://borisignatov1.pythonanywhere.com/users/integration", data=body,
                               auth=(user.username, user.password))
         #print(r.text)
     return r.text
